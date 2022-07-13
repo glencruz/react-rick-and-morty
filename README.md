3f0c0d185e5492c713cf9c1985ae49b4ccfeec88
glencruz <glen_cruzcanul@outlook.com>
2022-07-12 22:40:26 -0500

    Primer commit/push
    Primer push 12/07/2022
    Configuración dev y prod

---
+++ README.md
@@ -0,0 +1,16 @@
+# Proyecto React + Webpack + SASS
+
+# Inicializar el proyecto
+npm init -y
+git init
+npm run build
+
+# Instalaciones necesarias
+npm i core-js react react-dom
+npm i -D @babel/core @babel/preset-env @babel/preset-react @pmmmwh/react-refresh-webpack-plugin babel-loader clean-webpack-plugin css-loader html-webpack-plugin mini-css-extract-plugin react-refresh sass sass-loader style-loader webpack webpack-cli webpack-dev-server webpack-merge
+
+# PARA INSTALAR
+ npm install
+ npm run dev
+ 
+
---
+++ config/webpack.dev.js
@@ -0,0 +1,23 @@
+const { HotModuleReplacementPlugin } = require('webpack');
+const { ReactRefreshWebpackPlugin } = require('@pmmmwh/react-refresh-webpack-plugin');
+const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+
+/** @type {import('webpack').Configuration} */
+const devConfig = {
+    mode: "development",
+    devServer: {
+        port: 3000,
+        contentBase: "../dist",
+        open: "chrome",
+        hot: true
+    },
+    target: "web",
+    plugins: [
+        new HotModuleReplacementPlugin(),
+        new ReactRefreshWebpackPlugin()
+    ],
+    devtool: "eval-source-map"
+}
+
+module.exports = merge(common, devConfig)
